1d4265d4dbf8ab58a1c08956ab49abcc575730ae
Carlos <nicolaugalves@gmail.com>
2020-07-26 20:10:22 -0300

    ok ajustes

--- app/src/main/java/carlos/nicolau/galves/androidcwb/presentation/login/LoginPresenterImpl.kt
+++ app/src/main/java/carlos/nicolau/galves/androidcwb/presentation/login/LoginPresenterImpl.kt
@@ -3,10 +3,14 @@ package carlos.nicolau.galves.androidcwb.presentation.login
 import carlos.nicolau.galves.core.interators.IGetUserUseCase
 import carlos.nicolau.galves.utils.BasePresenter
 
-class LoginPresenterImpl(private val getUserUserCase: IGetUserUseCase)
+class LoginPresenterImpl(
+    private val getUserUserCase: IGetUserUseCase
+)
     : BasePresenter<LoginPresenter.View>(), LoginPresenter.Presenter {
 
     override fun onClickButtonLogin(username: String, password: String) {
+        mView?.showLoading()
         val user = getUserUserCase.execute(username, password)
+        mView?.hideLoading()
     }
 }
